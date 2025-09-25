89bbbb10f5617329af6a51d996629273e5bb8a69
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-09-26 03:38:07 +0500

    feat: implement isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -205,8 +205,17 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2 || n === 3) return true;
+
+  let i = 2;
+  while (i <= Math.sqrt(n)) {
+    if (n % i === 0) return false;
+    i += 1;
+  }
+
+  return true;
 }
 
 /**

7019096305642f852d4ed1ffb4b97c6d8d472530
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-09-26 04:18:52 +0500

    feat: implement getIntegerPartNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -569,8 +569,8 @@ function roundToNearestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
 }
 
 /**

1baaa2f2b9e4b8fc30b695e95cb5e2417dba98e6
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-09-26 04:17:38 +0500

    feat: implement roundToLargestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -539,8 +539,8 @@ function roundToSmallestInteger(number) {
  * 5.1  => 6
  * -5.9 => -5
  */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
